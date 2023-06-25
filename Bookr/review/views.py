f784a7f9a617ea3584887189dc6b94fda98d9aa4
Abdul Muizz <azikumapayi@gmail.com>
2023-06-25 01:44:46 +0000

    add a new function called search_string that takes a text and return the inputed text on the search.html page

--- Bookr/review/views.py
+++ Bookr/review/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render( request, "index.html")
+
+def search_result(request):
+
+    search_text = request.GET.get("search", "")
+
+    return render( request, "search.html", {"search_text":search_text})
